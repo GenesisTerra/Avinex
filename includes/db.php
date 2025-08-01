d88342c629f9c40f0ae703840f88e50162238b87
Aman Hegde <amanhegde2527@gmail.com>
2025-07-31 23:44:54 -0700

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "db.fr-pari1.bengt.wasmernet.com";
+$username = "c5fe5b077ab88000829f849ea1b3";
+$password = "0688c5fe-5b07-7bf3-8000-f3f9a4b49950";
 $dbname = "webapp";
 
 try {
